911a9e93525e60626592da1a6cc9b077bf735662
forkingits1123 <lblack11235813@gmail.com>
2021-05-20 14:08:04 -0700

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+   
+   bestCustomer = 'not bob';
+
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "Larry";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "not Larry";
+}
